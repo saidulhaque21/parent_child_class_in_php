34c29108295093f39638218f8a59b93bbbe4fc52
Md Saidul Haque <pclanguage@gmail.com>
2023-06-11 21:04:06 +0600

    Update example.php
    sample

--- example.php
+++ example.php
@@ -1,1 +1,30 @@
+<?php 
+ error_reporting(E_ALL);
+ ini_set("display_errors", 1);
 
+ 
+include 'lib/Parent_child_relation.php';
+include 'data/accounting_data.php';
+echo "<pre>";
+$parent_relation = new Parent_child_relation(); 
+
+      
+
+        // get tree 
+        //  $data = $parent_relation->get($accounting_data, ["type" => "tree"]);
+       $data = $parent_relation->get_tree($accounting_data);
+      
+       //get list 
+        //  $data = $parent_relation->get($accounting_data, ["type" => "list"]);
+        //   $data = $parent_relation->get_list($accounting_data);
+        
+        //get optgroup 
+        //  $data = $parent_relation->get($accounting_data, ["type" => "optgroup"]);
+        //   $data = $parent_relation->get_optgroup($accounting_data);
+       
+        //get optgroup 
+        //  $data = $parent_relation->get($accounting_data, ["type" => "dropdown"]);
+      //  $data = $parent_relation->get_dropdown($accounting_data);
+        print_r($data);
+
+?>
